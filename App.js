f84bc11ad501c951769ceef19264b5896cf4c418
Jonna Huotari <t1hujo04@students.oamk.fi>
2023-11-07 16:05:06 +0200

    navigointia aloitettu

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LogIn from './Screens/LogIn';
+import Registration from './Screens/Registration'
+import DefaultView from './Screens/DefaultView'
+import Favorites from './Screens/Favorites'
+import GroceryList from './Screens/GroceryList'
+import MenuList from './Screens/MenuList'
+import Recipe from './Screens/Recipe'
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+    <SafeAreaView style={styles.container}>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='LogIn' 
+    screenOptions={{headerShown: false}}>
+  <Stack.Screen name='LogIn'>
+  {(props) =>
+      <LogIn
+      {...props}
+      title="LogIn"
+      />
+  }
+    </Stack.Screen>
+    <Stack.Screen name='registration'>
+    {(props) =>
+      <Registration
+        {...props}
+      title="Registration"
+      />
+    }
+    </Stack.Screen>
+
+    <Stack.Screen name='defaultView'>
+    {(props) =>
+       <DefaultView
+       {...props}
+       title="Defaulview"
+       />
+    }
+    </Stack.Screen>
+
+    <Stack.Screen name='menuList'>
+    {(props) =>
+       <MenuList
+       {...props}
+       title="Menulist"
+       />
+    }
+    </Stack.Screen>
+
+    <Stack.Screen name='recipe'>
+    {(props) =>
+       <Recipe
+       {...props}
+       title="Recipe"
+       />
+    }
+    </Stack.Screen>
+
+    <Stack.Screen name='groceryList'>
+    {(props) =>
+       <GroceryList
+       {...props}
+       title="Grocerylist"
+       />
+    }
+    </Stack.Screen>
+
+    <Stack.Screen name='favorites'>
+    {(props) =>
+       <Favorites
+       {...props}
+       title="Favorites"
+       />
+    }
+    </Stack.Screen>
+  </Stack.Navigator>
+  </NavigationContainer>
+  </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
 });
---
+++ Screens/GroceryList.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GroceryList() {
+  return (
+    <View>
+      <Text>GroceryList</Text>
+    </View>
+  )
+}
