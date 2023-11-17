38154a633c4efddc30cff55c903c869d3022ea07
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-11-17 10:54:12 +0200

    Lisätty Navigointi ja HomeScreen raakile

--- Screens/MenuList.js
+++ Screens/MenuList.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/core'
 
-export default function MenuList() {
+const MenuList = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>MenuList</Text>
+    <View style={styles.container}>
+      <Text>Toka sivu</Text>
+      <Text style={styles.headerText}>Ateriat</Text>
+      <Button
+        title='HomeScreen button'
+        onPress={() => navigation.navigate('Home')} />
     </View>
   )
 }
+
+export default MenuList
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerText: {
+    fontSize: 30,
+    margin: 20
+  },
+})
