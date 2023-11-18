d9ccef48bb5b39f29e9a1847fc23d52b1a236641
Henna <henna.hannila@gmail.com>
2023-11-18 15:40:18 +0200

    Ensimmäinen ateria tulostuu

--- Screens/MenuList.js
+++ Screens/MenuList.js
@@ -1,35 +1,74 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
-import React from 'react'
-import { useNavigation , useRoute } from '@react-navigation/core'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/core';
+import { firestore } from '../firebase';
 
 const MenuList = () => {
-  const navigation = useNavigation()
-  const route = useRoute()
-  const { selectedCategory } = route.params
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { selectedCategory } = route.params;
+  const [menuData, setMenuData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `https://meal-base-99bc5-default-rtdb.firebaseio.com/Kategoriat/${selectedCategory}.json`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setMenuData(data);
+        console.log(url);
+        console.log('Fetched menu data:', data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const receiptName = menuData ? Object.keys(menuData.Reseptit)[0] : null;
+
+  const handleReceiptPress = () => {
+    if (receiptName) {
+      navigation.navigate('Recipe', { receiptName });
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Toka sivu</Text>
-      <Text style={styles.headerText}>Ateriat</Text>
-      <Text style={styles.headerText}>{selectedCategory}</Text>
-      <Button
-        title='HomeScreen button'
-        onPress={() => navigation.navigate('Home')} />
+      <Text style={styles.headerText}>Menu Data</Text>
+      <View style={styles.contentContainer}>
+        {receiptName ? (
+          <TouchableOpacity onPress={handleReceiptPress}>
+            <Text>{receiptName}</Text>
+          </TouchableOpacity>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default MenuList
+export default MenuList;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
+  },
+  headerContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginBottom: 20,
   },
   headerText: {
     fontSize: 30,
-    margin: 20
   },
-})
-
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
